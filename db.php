43e2edaf3313e1d06bcb14ba4db0d99cfc0c927d
Chirag30Sharma <chirag30sharma@gmail.com>
2023-07-13 14:18:28 +0530

    database updated

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $conn=mysqli_connect("bloodbond.cy1tpuc1yvet.us-west-1.rds.amazonaws.com","admin","Bloodbond123","login");
 
-// if($conn){
-//     echo "connection successfull";
-// }else{
-//     echo "connection error".mysqli_connect_error();
-// }
+if($conn){
+    echo "connection successfull";
+}else{
+    echo "connection error".mysqli_connect_error();
+}
 
 ?>
