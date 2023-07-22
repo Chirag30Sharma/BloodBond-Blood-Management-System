de3e4bd62473362ac64feaade6b24dfb1a88d133
Vidhi-B-Shah <vidhivk18@gmail.com>
2023-07-22 15:26:18 +0530

    Fixed Bookings page

--- admin/bookings.php
+++ admin/bookings.php
@@ -1,74 +1,80 @@
 <?php
 include('inc/db.php');
 session_start();
-// If the user is not logged in redirect to the login page...
+// If the user is not logged in, redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.php');
-	exit;
+    header('Location: index.php');
+    exit;
 }
 
 $email = $_SESSION["email"];
 
+// Get the organization name using the email
 $sql = "SELECT org_name FROM admin_registration WHERE org_email = '$email'";
 $result = mysqli_query($conn, $sql);
 
 if ($result && mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
     $name = $row['org_name'];
+} else {
+    echo "Organization not found!";
+    exit;
 }
-
 
 $stmt = $conn->prepare("SELECT date, address, user_email, user_name FROM donate WHERE org_name = ?");
 $stmt->bind_param("s", $name);
-$stmt->execute();
 
+// Execute the query and check for errors
+if (!$stmt->execute()) {
+    die("Error executing the query: " . $stmt->error);
+}
+
+// Get the result and check if there are rows
 $result = $stmt->get_result();
 
+// Initialize variables for booking details
+$bookingDetails = array();
+
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $date = $row['date'];
-        $address = $row['address'];
-        $user_email = $row['user_email'];
-        $user_name = $row['user_name'];
-        
+    while ($row = $result->fetch_assoc()) {
+        $bookingDetails[] = array(
+            'date' => $row['date'],
+            'address' => $row['address'],
+            'user_email' => $row['user_email'],
+            'user_name' => $row['user_name']
+        );
     }
-
 } else {
-    echo "0 results";
-} 
+    echo "No booking details found for the organization: $name";
+}
 
 
-$sql1 = "SELECT name FROM login WHERE email = '$user_email'";
-$result1 = mysqli_query($conn, $sql1);
-$stmt = $conn->prepare("SELECT dob, mobileno, gender, bloodgroup, locality FROM login WHERE email = ?");
-$stmt->bind_param("s", $user_email);
-$stmt->execute();
+// Fetch user details for each booking
+$userDetails = array();
 
-$result1 = $stmt->get_result();
+foreach ($bookingDetails as $booking) {
+    $user_email = $booking['user_email'];
 
-if ($result1->num_rows > 0) {
-    while($row = $result1->fetch_assoc()) {
-        $dob = $row['dob'];
-        $mobileno = $row['mobileno'];
-        $gender = $row['gender'];
-        $bloodgroup = $row['bloodgroup'];
-        $locality = $row['locality'];
-        
+    $stmt = $conn->prepare("SELECT dob, mobileno, gender, bloodgroup, locality FROM login WHERE email = ?");
+    $stmt->bind_param("s", $user_email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $userDetails[$user_email] = array(
+            'dob' => $row['dob'],
+            'mobileno' => $row['mobileno'],
+            'gender' => $row['gender'],
+            'bloodgroup' => $row['bloodgroup'],
+            'locality' => $row['locality']
+        );
     }
-
-} else {
-    echo "0 results";
-} 
+}
 
 
-// if(isset($_POST["confirm"])){
-//     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-//     }
-// }
-
-     
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -85,7 +91,7 @@ if ($result1->num_rows > 0) {
         }
 
         .container {
-            max-width: 800px;
+            max-width: 1200px; /* Limit the maximum width of the container */
             margin: 20px auto;
             padding: 20px;
             background-color: #fff;
@@ -102,6 +108,7 @@ if ($result1->num_rows > 0) {
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
             gap: 20px;
+            margin-top: 20px;
         }
 
         .card {
@@ -119,10 +126,6 @@ if ($result1->num_rows > 0) {
 
         .card p {
             margin: 5px 0;
-        }
-
-        .booking-cards {
-            margin-top: 20px;
         }
 
         .booking-cards .card {
@@ -144,21 +147,39 @@ if ($result1->num_rows > 0) {
     </style>
 </head>
 <body>
-    <div class="container">
+<div class="container">
         <h1>Bookings Received</h1>
         <div class="booking-cards">
-            <div class="card">
-                <h2><?php echo $name ?></h2>
-                <h3>Person Details</h3>
-                <p><strong>Name:</strong> <?php echo $user_name ?></p>
-                <p><strong>Email:</strong> <?php echo $user_email ?></p>
-                <p><strong>Phone:</strong> <?php echo $mobileno ?></p>
-                <p><strong>Blood Group:</strong> <?php echo $bloodgroup ?></p>
-                <p><strong>Gender:</strong> <?php echo $gender ?></p>
-                <h3>Event Details</h3>
-                <p><strong>Date:</strong> <?php echo $date ?></p>
-                <p><strong>Address:</strong> <?php echo $address ?></p>
-            </div>
+            <?php
+            // Display booking information in cards
+            foreach ($bookingDetails as $booking) {
+                $user_email = $booking['user_email'];
+                $user_name = $booking['user_name'];
+
+                echo '<div class="card">';
+                echo "<h2>{$name}</h2>";
+                echo '<h3>Person Details</h3>';
+                echo "<p><strong>Name:</strong> {$user_name}</p>";
+                echo "<p><strong>Email:</strong> {$user_email}</p>";
+
+                // Check if user details are available for this booking
+                if (isset($userDetails[$user_email])) {
+                    $userDetail = $userDetails[$user_email];
+                    echo "<p><strong>Date of Birth:</strong> {$userDetail['dob']}</p>";
+                    echo "<p><strong>Phone:</strong> {$userDetail['mobileno']}</p>";
+                    echo "<p><strong>Blood Group:</strong> {$userDetail['bloodgroup']}</p>";
+                    echo "<p><strong>Gender:</strong> {$userDetail['gender']}</p>";
+                    echo "<p><strong>Locality:</strong> {$userDetail['locality']}</p>";
+                } else {
+                    echo '<p><strong>User details not found.</strong></p>';
+                }
+
+                echo '<h3>Event Details</h3>';
+                echo "<p><strong>Date:</strong> {$booking['date']}</p>";
+                echo "<p><strong>Address:</strong> {$booking['address']}</p>";
+                echo '</div>';
+            }
+            ?>
         </div>
     </div>
 </body>
